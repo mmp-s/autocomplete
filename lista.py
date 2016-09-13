5746ab07b4e43b51bbbd16fcc04a21f7c93209af
MateusPonciano <mmps@cin.ufpe.br>
2016-09-13 13:14:40 -0300

    att

--- lista.py
+++ lista.py
@@ -1,3 +1,11 @@
+def cmp(atual, item):
+    if item == atual:
+        return '0'
+    elif item < atual:
+        return '<0'
+    else:
+        return '>0'
+
 class No:
     def __init__(self,item=None,ant=None,prox=None):
         self.item = item
@@ -12,6 +20,9 @@ class Lista:
     
     def size(self):
         return self.tamanho
+
+    def vazia(self):
+        return self.primeiro == self.ultimo
     
     #TODO: implemente
     def inserirOrdenado(self, item, cmp):    
@@ -20,6 +31,28 @@ class Lista:
         cmp: funcao de comparacao que retorna <0, 0 ou >0 se primeiro valor
             for menor, igual ou maior que o segundo valor 
         '''
+        atual = self.item
+
+        if self.vazia():
+            elemento = No()
+            elemento.item = item
+            self.ultimo.prox = elemento
+            self.ultimo = elemento
+            elemento.ant = self.primeiro
+
+        elif cmp(atual, item) == '0': self.item = item
+
+        elif cmp(atual, item) == '<0': inserirOrdenado(self.prox, item, cmp)
+
+        else:
+            elemento = No()
+            elemento.item = item
+            anterior = self.ant
+            self.ant = elemento
+            elemento.prox = self
+            elemento.ant = anterior
+            anterior.prox = elemento
+        
         pass
     
     #TODO: implemente    
